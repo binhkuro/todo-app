1bce1540fee8697d9a7e5091925b2962b7f5b22a
Cao Nguyên Bình <caonguyenbinh12@gmail.com>
2024-05-14 09:36:53 +0700

    app đơn giản

--- src/components/TodoSwitchScreen.js
+++ src/components/TodoSwitchScreen.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
 
-class TodoSwitchScreen extends Component {
-    render() {
-        const { isCompletedScreen, onSwitchScreen } = this.props;
-        return (
-            <div className="btn-area">
-                <button
-                    className={`isCompletedScreen ${!isCompletedScreen ? 'active' : ''}`}
-                    onClick={() => onSwitchScreen(false)}
-                >
-                    Todo
-                </button>
-                <button
-                    className={`isCompletedScreen ${isCompletedScreen ? 'active' : ''}`}
-                    onClick={() => onSwitchScreen(true)}
-                >
-                    Completed
-                </button>
-            </div>
-        );
-    }
+function TodoSwitchScreen({ isCompletedScreen, onSwitchScreen }) {
+    return (
+        <div className="btn-area">
+            <button
+                className={`isCompletedScreen ${!isCompletedScreen ? 'active' : ''}`}
+                onClick={() => onSwitchScreen(false)}
+            >
+                Todo
+            </button>
+            <button
+                className={`isCompletedScreen ${isCompletedScreen ? 'active' : ''}`}
+                onClick={() => onSwitchScreen(true)}
+            >
+                Completed
+            </button>
+        </div>
+    );
 }
 
 export default TodoSwitchScreen;
